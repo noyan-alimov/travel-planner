2fb213523a7e523bf79889214696f3c0a12cd2c4
Noyan Alimov <noyan.alimov@gmail.com>
2020-05-25 20:19:46 +0100

    set up our first API weatherbit

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,11 @@
 import './styles/styles.scss';
+import { fetchWeather } from './js/fetchWeather';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const apiKey = '76da7ec1199a4d71964e8ecf8c8f2f6d';
+  const city = document.querySelector('#city').value;
+  fetchWeather(apiKey, city);
+});
--- src/client/js/fetchWeather.js
+++ src/client/js/fetchWeather.js
@@ -1,13 +1,9 @@
-const fetch = require('node-fetch');
-
-const apiKey = '76da7ec1199a4d71964e8ecf8c8f2f6d';
-
-const fetchWeather = async apiKey => {
-  const res = await fetch(
-    `http://api.weatherbit.io/v2.0/current?city=London&key=${apiKey}`
-  );
-  const data = await res.json();
-  console.log(data.data[0].temp);
+export const fetchWeather = (apiKey, city) => {
+  fetch(`http://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.data[0].temp);
+      const output = document.querySelector('.output');
+      output.innerHTML = `Temperature: ${data.data[0].temp}°C`;
+    });
 };
-
-fetchWeather(apiKey);
